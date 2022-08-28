98d91cc76487afd01a39596e711ddb95db9b8a01
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-08-28 11:13:55 +0000

    Deploying to gh-pages from @ Amfiteatr42/G5-IceCream@2f417df517af8ff94bed2626bb9b9bf6edd83d21 🚀

---
+++ index.398d69b4.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[about-modal-open]"),closeModalBtn:document.querySelector("[about-modal-close]"),modal:document.querySelector("[about-modal]")};function e(){o.modal.classList.toggle("abot-section__modal-backdrop--hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.398d69b4.js.map
