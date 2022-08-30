53054350325ce637217bbe13af8006c7be55d427
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-08-30 06:48:48 +0000

    Deploying to gh-pages from @ Amfiteatr42/G5-IceCream@e4b449143ec9c416c7cb08fdc8cf018760a00663 🚀

--- index.206ce81e.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[products-overlay-open]"),closeModalBtn:document.querySelector("[products-overlay-close]"),modal:document.querySelector("[products-overlay]")};o.openModalBtn.addEventListener("click",(function(){o.modal.classList.toggle("products__overlay--hidden")}))}();
-//# sourceMappingURL=index.206ce81e.js.map
--- index.3d93fc94.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[products-overlay-open3]"),closeModalBtn:document.querySelector("[products-overlay-close3]"),modal:document.querySelector("[products-overlay3]")};o.openModalBtn.addEventListener("click",(function(){o.modal.classList.toggle("products__overlay--hidden3")}))}();
-//# sourceMappingURL=index.3d93fc94.js.map
